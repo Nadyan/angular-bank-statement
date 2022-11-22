d18c0d76d304cd0407f2b988a503d3e348ee6d03
Nadyan <nadyan_sp@hotmail.com>
2022-11-22 18:27:20 -0300

    statement page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { LOCALE_ID, DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ExtratoComponent } from './extrato/extrato.component';
 import { TransferenciaComponent } from './transferencia/transferencia.component';
+import localePt from '@angular/common/locales/pt'
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -16,7 +20,10 @@ import { TransferenciaComponent } from './transferencia/transferencia.component'
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'},
+    {provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
